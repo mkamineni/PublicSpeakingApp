a8969520915413f37bc7c80da1e0463e86433d87
Shannen Wu <shannen@mit.edu>
2018-09-15 17:55:08 -0400

    set up main.py and added fetch in App.js

--- backend-src/main.py
+++ backend-src/main.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, request
+from transcribe import process_audio
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
-@app.route('/')
-def something():
+@app.route('/send', methods=['POST'])
+def send_video():
+	body = request.get_json()
+	#may need to process body['form']
+	#call function
     return "setup is hard"
--- react-src/App.js
+++ react-src/App.js
@@ -2,6 +2,44 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: '',
+      msg: '',
+      data: []
+    };
+  }
+  sendVideo(page, values) {
+    this.setState({ loading: 'sendVideo'});
+    const body = { form: values};
+    fetch(`/api/send`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(body),
+    })
+      .then(res => res.json())
+      .then(
+        obj => {
+          const data = obj.data;
+          this.setState({
+            loading: '',
+            msg: 'Video sent'
+            data: [ obj.data ]
+          });
+        },
+        error => {
+          this.setState({
+            loading: '', 
+            msg: 'Error sending video'
+          });
+          console.error(error);
+        }
+      );
+  }
+
   render() {
     const styles = StyleSheet.create({
       container: {
@@ -11,7 +49,6 @@ export default class App extends React.Component {
         justifyContent: 'center',
       },
     });
-
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
