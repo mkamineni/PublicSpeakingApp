b716327d4fb8e3e143ff29a7ab10c6aa6b657166
Shannen Wu <shannen@mit.edu>
2018-09-16 05:22:51 -0400

    connect

--- backend-src/main.py
+++ backend-src/main.py
@@ -3,12 +3,14 @@ app = Flask(__name__)
 app.config['DEBUG'] = True
 app.config['TESTING'] = True
 
+from transcribe import audio_to_text
+
 @app.route('/')
 def hello_world():
 	return "hi"
 
-@app.route('/analyze', methods=['GET', 'POST'])
+@app.route('/analyze', methods=['POST'])
 def analyze_video():
-	app.logger.info(request)
 	body = request.get_json()
-	return {"boop":"boop"}
+    output = audio_to_text(body)
+	return output
--- backend-src/transcribe.py
+++ backend-src/transcribe.py
@@ -1,21 +1,21 @@
 import requests
 from time import sleep
 
-filler_file="filler_words.txt"
-filler_words=set()
+filler_file = "filler_words.txt"
+filler_words = set()
 with open(filler_file, "r+") as f:	
 	for line in f:
 		filler_words.add(line)
 
-def audio_to_text(ID=None):
+def audio_to_text(ID = None):
 	'''
 	'''
-	new_headers= {
+	new_headers = {
 	   	'Authorization': 'Bearer 01wXudOy88gdkI2HgWeZUrc1WEp5GtFA5XK9KFNv6O7ucF6e5Nwe47CXm3cAbMAFHML56xeOQy0Ya99FFVwF7dqrzitwc',
 	   	'Accept': 'application/vnd.rev.transcript.v1.0+json',
 	}
 
-	if ID==None:
+	if ID == None:
 		headers = {
 		    'Authorization': 'Bearer 01wXudOy88gdkI2HgWeZUrc1WEp5GtFA5XK9KFNv6O7ucF6e5Nwe47CXm3cAbMAFHML56xeOQy0Ya99FFVwF7dqrzitwc',
 		    'Content-Type': 'application/json',
@@ -27,29 +27,29 @@ def audio_to_text(ID=None):
 		ID=response["id"]
 
 		print(ID)
-		status=response["status"]
-		while status!='transcribed':
+		status = response["status"]
+		while status != 'transcribed':
 			sleep(30)
 			response = requests.get('https://api.rev.ai/revspeech/v1beta/jobs/'+str(ID), headers=new_headers).json()
-			status=response["status"]
+			status = response["status"]
 
 	response2 = requests.get('https://api.rev.ai/revspeech/v1beta/jobs/'+str(ID)+'/transcript', headers=new_headers).json()
 
-	text=parse_response(response2)
+	text = parse_response(response2)
 	return text
 
 def parse_response(response2):
-	colors={"blue": set()}
-	text=[]
-	output=""
-	parts=response2["monologues"][0]["elements"]
+	colors = {"blue": set()}
+	text = []
+	output = ""
+	parts = response2["monologues"][0]["elements"]
 
 	for ind in range(len(parts)):
-		elem=parts[ind]
+		elem = parts[ind]
 		text.append(elem["value"])
-		output+=elem["value"]
+		output += elem["value"]
 		if elem["value"].lower() in filler_words:
-			print("F: "+elem["value"]+"\n")
+			print("F: " + elem["value"]+"\n")
 			colors["blue"].add(ind)
 
 	return text, output, colors
